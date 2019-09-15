cc3dc5040a8371a7a01c3d7d3c24fbba49b88bc1
Foxyf76 <lukefox5@gmail.com>
2019-09-15 20:45:49 +0100

    Finished up to vid #35

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,56 @@
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = formData;
+
+  // use on change for every field with e.target.name
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    console.log('Success');
+  };
+
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Sign In</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i>Sign into your Account
+      </p>
+      <form className='form' onSubmit={e => onSubmit(e)}>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            minLength='6'
+            value={password}
+            onChange={e => onChange(e)}
+          />
+        </div>
+      </form>
+      <p className='my-1'>
+        Don't have an account? <Link to='login.html'>Sign Up</Link>
+      </p>
+    </Fragment>
+  );
+};
+
+export default Login;
